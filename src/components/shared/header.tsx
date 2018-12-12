4e114d7a10726a4241eaab5ad92977e923632809
Edmund Mai <edmundmai@gmail.com>
2018-12-12 16:24:18 -0500

    Redesigns header and updates favicon

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -7,30 +7,50 @@ const ICON_LOGO = require("../../assets/images/logo.svg");
 
 const Container = styled.div`
   background: #fff;
-  border-bottom: 1px solid black;
   padding: 17px 32px;
 `;
 
 const Navbar = styled.div`
   height: 100%;
   display: flex;
+  justify-content: space-between;
 `;
 
-const LogoContainer = styled.div``;
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const LinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-const LogoLink = styled.a`
-  display: inline-block;
+const Link = styled.a`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: 0.4px;
+  color: #487aa7;
+  text-transform: uppercase;
+
+  &:hover {
+    text-decoration: none;
+    color: #487aa7;
+  }
 `;
 
 const Logo = styled.img`
   width: 28px;
   height: 28px;
   margin-right: 10px;
-  background: green;
 `;
 
 const CompanyName = styled.span`
-  font-family: ProximaNova;
+  text-transform: uppercase;
   font-size: 18px;
   font-weight: bold;
   font-style: normal;
@@ -44,11 +64,16 @@ const Header = () => (
   <Container>
     <Navbar>
       <LogoContainer>
-        <LogoLink title="Opacity Storage's Logo">
+        <Link title="Opacity Storage's Logo">
           <Logo src={ICON_LOGO} alt="logo" />
           <CompanyName>Opacity</CompanyName>
-        </LogoLink>
+        </Link>
       </LogoContainer>
+      <LinkContainer>
+        <Link href="https://www.opacity.io/" target="_blank">
+          Home
+        </Link>
+      </LinkContainer>
     </Navbar>
   </Container>
 );
