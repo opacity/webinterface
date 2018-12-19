2b7c2540c8f00fa2d255a09520db099ffa722d1b
Edmund <edmundmai@gmail.com>
2018-12-18 22:21:13 -0500

    Reskins download screens (#12)
    * reskins retrieve stage 1
    
    * reskins retrieve stage 2
    
    * change to opacity

--- src/components/download-form/download-form-slide.tsx
+++ src/components/download-form/download-form-slide.tsx
@@ -1,31 +1,67 @@
 import React from "react";
 import styled from "styled-components";
 
-import Slide from "../shared/slide";
 import Button from "../shared/button";
 import Spinner from "../shared/spinner";
 
 import { DOWNLOAD_STATUSES } from "../../config";
+const ICON_DOWNLOAD = require("../../assets/images/icon_download.svg");
 
-const HandleInstructions = styled.p`
-  padding-bottom: 150px;
+import ScreenContainer from "../shared/screen-container";
+import ScreenDescription from "../shared/screen-description";
+
+const HandleLabel = styled.h3`
+  font-size: 16px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: 0.7px;
+  color: #ffffff;
+  text-transform: uppercase;
 `;
 
-const HandleLabel = styled.span`
-  color: #6ea0de;
-  font-size: 20px;
-  margin-right: 15px;
+const Icon = styled.img`
+  height: 18px;
+  width: 20px;
+  margin-right: 10px;
+`;
+
+const InputContainer = styled.div`
+  margin-bottom: 60px;
 `;
 
 const HandleTextInput = styled.input`
+  color: #ffffff;
   border: none;
-  border-bottom: #0068ea 1px solid;
-  width: 40%;
+  width: 380px;
+  height: 25px;
+  background-color: #232b40;
 
   &:focus {
     outline: none !important;
     box-shadow: none !important;
   }
+`;
+
+const DownloadButton = styled(Button)`
+  align-items: center;
+  background-color: #846b99;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+  display: flex;
+  font-size: 14px;
+  font-stretch: normal;
+  font-style: normal;
+  font-weight: bold;
+  height: 40px;
+  justify-content: center;
+  letter-spacing: 0.4px;
+  line-height: normal;
+  margin-right: 20px;
+  text-transform: uppercase;
+  width: 300px;
 `;
 
 interface DownloadFormSlideProps {
@@ -52,16 +88,15 @@ class DownloadFormSlide extends React.Component<
 
   render() {
     const { download, status } = this.props;
-    const ICON_DOWNLOAD = require("../../assets/images/icon_download.png");
     return (
-      <Slide title="Retrieve a File" image={ICON_DOWNLOAD}>
-        <HandleInstructions>
-          Enter your Oyster handle below to access your stored file from the
+      <ScreenContainer title={"Retrieve a file"}>
+        <ScreenDescription>
+          Enter your Opacity handle below to access your stored file from the
           Tangle.
-        </HandleInstructions>
-        <div>
+        </ScreenDescription>
+        <InputContainer>
           <label>
-            <HandleLabel>Oyster Handle:</HandleLabel>
+            <HandleLabel>Enter Opacity Handle</HandleLabel>
             <HandleTextInput
               id="download-handle-input"
               name="handle"
@@ -69,11 +104,10 @@ class DownloadFormSlide extends React.Component<
               onChange={evt => this.inputHandler(evt)}
             />
           </label>
-        </div>
+        </InputContainer>
         <div>
-          <Button
+          <DownloadButton
             id="download-btn"
-            className="primary-button"
             disabled={status === DOWNLOAD_STATUSES.PENDING}
             onClick={() => {
               const handle = this.state.handle;
@@ -82,18 +116,18 @@ class DownloadFormSlide extends React.Component<
               } else {
                 download(handle);
               }
-            }}
-          >
+            }}>
+            <Icon src={ICON_DOWNLOAD} />
             {status === DOWNLOAD_STATUSES.PENDING
               ? "Retrieving file..."
               : "Retrieve File"}
-          </Button>
+          </DownloadButton>
           <Spinner
             isActive={status === DOWNLOAD_STATUSES.PENDING}
             className="download-spinner"
           />
         </div>
-      </Slide>
+      </ScreenContainer>
     );
   }
 }
