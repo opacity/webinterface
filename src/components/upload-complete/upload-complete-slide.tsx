4f102f4d042f8d2531b20fced35075b114bf3bec
Edmund Mai <edmundmai@gmail.com>
2018-12-20 16:34:42 -0500

    Reskins upload stage 5

--- src/components/upload-complete/upload-complete-slide.tsx
+++ src/components/upload-complete/upload-complete-slide.tsx
@@ -1,27 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
-import ClipboardBtn from "../shared/clipboard-button";
-import Slide from "../shared/slide";
+import ClipboardWidget from "../shared/clipboard-widget";
+import ScreenContainer from "../shared/screen-container";
+import ScreenDescription from "../shared/screen-description";
 
-const ICON_READY = require("../../assets/images/icon_ready.png");
+const Terminology = styled.span`
+  color: #846b99;
+  font-weight: 600;
+`;
 
 const UploadCompleteSlide = ({ handle }) => (
-  <Slide title="Upload Complete" image={ICON_READY}>
-    <p className="complete-instructions">
-      Your file has been successfully uploaded to the Tangle. An Oyster handle
-      has been generated below. This handle is the only way to access your file
-      on the Tangle. Please store this handle in a safe place.
-    </p>
-    <div>
-      <p>
-        <span className="handle-header">Oyster Handle:</span>
-      </p>
-      <p id="oyster-handle" className="oyster-handle">
-        {handle}
-      </p>
-      <ClipboardBtn text={handle}>Copy to clipboard</ClipboardBtn>
-    </div>
-  </Slide>
+  <ScreenContainer title={"Attachment Complete"}>
+    <ScreenDescription>
+      Your file has been successfully uploaded to the Tangle. Your{" "}
+      <Terminology>Opacity Handle</Terminology> is below. This handle is the
+      only way to access your file on the Tangle. Please store this handle in a
+      safe place.
+    </ScreenDescription>
+    <ClipboardWidget text={handle} />
+  </ScreenContainer>
 );
 
 export default UploadCompleteSlide;
