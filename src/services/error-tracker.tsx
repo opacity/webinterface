ebc99d723d7f222b0570649ea3d009cb52226374
Edmund Mai <edmundmai@gmail.com>
2018-12-31 17:06:06 -0500

    update

--- src/services/error-tracker.tsx
+++ src/services/error-tracker.tsx
@@ -1,17 +1,20 @@
 import Raven from "raven-js";
-import _ from "lodash";
 
 Raven.config(
   "https://8fdbdab452f04a43b5c3f2e00ec126f7@sentry.io/295597"
 ).install();
 
 let lastAlertShownAt: number;
+const REQUIRED_MS_BETWEEN_ALERTS = 1000;
 
 export const alertUser = err => {
   Raven.captureException(err);
 
   const now = +new Date();
-  if (_.isUndefined(lastAlertShownAt) || now - lastAlertShownAt > 1000) {
+  if (
+    lastAlertShownAt === undefined ||
+    now - lastAlertShownAt > REQUIRED_MS_BETWEEN_ALERTS
+  ) {
     window.alert(err);
     lastAlertShownAt = +new Date();
   }
