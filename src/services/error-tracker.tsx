b59b3266cfd2894651d63ac546dd154b0d0ec0cb
Edmund Mai <edmundmai@gmail.com>
2018-12-31 13:11:21 -0500

    throttle alerts

--- src/services/error-tracker.tsx
+++ src/services/error-tracker.tsx
@@ -1,12 +1,20 @@
 import Raven from "raven-js";
+import _ from "lodash";
 
 Raven.config(
   "https://8fdbdab452f04a43b5c3f2e00ec126f7@sentry.io/295597"
 ).install();
 
+let lastAlertShownAt: number;
+
 export const alertUser = err => {
   Raven.captureException(err);
-  window.alert(err);
+
+  const now = +new Date();
+  if (_.isUndefined(lastAlertShownAt) || now - lastAlertShownAt > 1000) {
+    window.alert(err);
+    lastAlertShownAt = +new Date();
+  }
 };
 
 export default Raven;
