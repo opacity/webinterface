dd6631083bb4ca477b44d649e4cd91bd1251f4e5
William Halunen <whalunen@student.bridgew.edu>
2019-01-16 23:16:27 -0500

    Added QR Code of Payment Address to Payment Page

--- src/components/payment-invoice/payment-invoice-slide.tsx
+++ src/components/payment-invoice/payment-invoice-slide.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-
 import ScreenContainer from "../shared/screen-container";
 import ScreenDescription from "../shared/screen-description";
 import ClipboardWidget from "../shared/clipboard-widget";
+
+var QRCode = require('qrcode.react');
 
 const Cost = styled.span`
   color: #846b99;
@@ -18,8 +19,24 @@ const PaymentInvoiceSlide = ({ cost, ethAddress, gasPrice }) => (
       upload will start automatically after your payment is received. This may
       take some time depending on network traffic.
     </ScreenDescription>
+
+  <ScreenDescription>
+    <QRCode
+    value = {ethAddress}
+    size = "200"
+    renderAs = "svg"
+    bgColor="#ffffff"
+    fgColor="#2e3854"
+    level = "H"
+    color = "#ffffff"
+    includeMargin = "true"
+    />
+   </ScreenDescription>
+
     <ClipboardWidget title="Address" text={ethAddress} property="Address" />
+
   </ScreenContainer>
 );
+
 
 export default PaymentInvoiceSlide;
