47b211998f9e29e4642f33b50d4798955066cb39
CKH4 <connorhay@protonmail.com>
2019-01-29 03:08:02 -0500

    Update config

--- tslint.config.js
+++ tslint.config.js
@@ -21,6 +21,27 @@ module.exports = {
   rules: {
     "space-before-function-paren": [true, "always"],
     "arrow-parens": [true, "ban-single-arg-parens"],
+    "no-unexpected-multiline": true,
+    "ter-no-irregular-whitespace": [true],
+    "ter-indent": [
+      true,
+      2,
+      {
+        "SwitchCase": 1,
+        "MemberExpression": 1,
+        "FunctionDeclaration": {
+          "parameters": 1,
+          "body": 1
+        },
+        "FunctionExpression": {
+          "parameters": 1,
+          "body": 1
+        },
+        "CallExpression": {
+          "arguments": 1
+        }
+      }
+    ],
     semicolon: [true, "always"],
     quotemark: [
       true,
@@ -29,12 +50,17 @@ module.exports = {
       "avoid-template",
       "avoid-escape"
     ],
+    "indent": [
+      true,
+      "spaces",
+      2
+    ],
     whitespace: [
       true,
       "check-branch",
       "check-decl",
       "check-operator",
-      // "check-module",
+      "check-module",
       "check-rest-spread",
       "check-type",
       "check-typecast",
