95303edc13aadf6f5e40d6ad7bcdc2ec9a5d9f62
Edmund Mai <edmundmai@gmail.com>
2019-01-31 13:04:41 -0500

    fix errors with qr code

--- src/components/payment-invoice/payment-invoice-slide.tsx
+++ src/components/payment-invoice/payment-invoice-slide.tsx
@@ -25,13 +25,13 @@ const PaymentInvoiceSlide = ({ cost, ethAddress, gasPrice, openMetamask }) => (
     <ScreenDescription>
       <QRCode
         value={ethAddress}
-        size="200"
+        size={200}
         renderAs="svg"
         bgColor="#ffffff"
         fgColor="#2e3854"
         level="H"
         color="#ffffff"
-        includeMargin="true"
+        includeMargin={true}
       />
     </ScreenDescription>
     <ClipboardWidget title="Address" text={ethAddress} property="Address" />
