c1f6108604b421869dee74db59a76577c922f3ab
Edmund Mai <edmundmai@gmail.com>
2019-02-04 19:19:23 -0500

    styles metamask button

--- src/components/payment-invoice/payment-invoice-slide.tsx
+++ src/components/payment-invoice/payment-invoice-slide.tsx
@@ -6,9 +6,22 @@ import ScreenDescription from "../shared/screen-description";
 import ClipboardWidget from "../shared/clipboard-widget";
 import QRCode from "qrcode.react";
 
+const PAY_WITH_METAMASK = require("../../assets/images/pay_with_metamask.png");
+
 const Cost = styled.span`
   color: #846b99;
   font-weight: 600;
+`;
+
+const MetamaskButton = styled.button`
+  cursor: pointer;
+  padding: 0;
+  border: none;
+  background: none;
+`;
+
+const MetamaskImg = styled.img`
+  width: 180px;
 `;
 
 const PaymentInvoiceSlide = ({ cost, ethAddress, gasPrice, openMetamask }) => (
@@ -19,9 +32,11 @@ const PaymentInvoiceSlide = ({ cost, ethAddress, gasPrice, openMetamask }) => (
       upload will start automatically after your payment is received. This may
       take some time depending on network traffic.
     </ScreenDescription>
-    <button onClick={() => openMetamask({ cost, ethAddress, gasPrice })}>
-      Open metamask
-    </button>
+    <MetamaskButton
+      onClick={() => openMetamask({ cost, ethAddress, gasPrice })}
+    >
+      <MetamaskImg src={PAY_WITH_METAMASK} />
+    </MetamaskButton>
     <ScreenDescription>
       <QRCode
         value={ethAddress}
