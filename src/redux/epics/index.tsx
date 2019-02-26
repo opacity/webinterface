74b1a9ee51be38101ddaf0d1348ed702b5622242
Edmund Mai <edmundmai@gmail.com>
2019-02-26 17:02:35 -0500

    extract all metamask stuff into separate redux flow

--- src/redux/epics/index.tsx
+++ src/redux/epics/index.tsx
@@ -3,11 +3,13 @@ import { combineEpics } from "redux-observable";
 import uploadEpic from "./upload-epic";
 import downloadEpic from "./download-epic";
 import downloadUploadHistoryEpic from "./download-upload-history-epic";
+import metamaskEpic from "./metamask-epic";
 import navigationEpic from "./navigation-epic";
 
 export default combineEpics(
   uploadEpic,
   downloadEpic,
   downloadUploadHistoryEpic,
+  metamaskEpic,
   navigationEpic
 );
