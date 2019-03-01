23bb878598f43effb9470c7778de7464f27b44db
Edmund Mai <edmundmai@gmail.com>
2019-03-01 12:37:06 -0500

    internal stateful routing for sign up

--- src/components/signup/create-account.tsx
+++ src/components/signup/create-account.tsx
@@ -1,18 +1,61 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { Component } from "react";
 
-// import ConfirmPaymentSlide from "./confirm-payment-slide";
-// import RecordStorageHandleSlide from "./record-storage-handle-slide";
-// import SendPaymentSlide from "./send-payment-slide";
-// import RetrievingInvoiceSlide from "./retrieving-invoice-slide";
+import RecordRecoveryPhraseSlide from "./record-recovery-phrase-slide";
+import RecordStorageHandleSlide from "./record-storage-handle-slide";
+import SendPaymentSlide from "./send-payment-slide";
+import ConfirmPaymentSlide from "./confirm-payment-slide";
 
-const CreateAccount = ({
-  handle,
-  cost,
-  ethAddress,
-  gasPrice,
-  setStoragePin,
-  setPrivateKey
-}) => <div />;
+const PHASES = {
+  RECORD_RECOVERY_PHRASE: "RECORD_RECOVERY_PHRASE",
+  RECORD_STORAGE_PIN: "RECORD_STORAGE_PIN",
+  SEND_PAYMENT: "SEND_PAYMENT",
+  CONFIRM_PAYMENT: "CONFIRM_PAYMENT"
+};
 
-export default connect()(CreateAccount);
+interface CreateAccountProps {
+  handle;
+  cost;
+  ethAddress;
+  gasPrice;
+  setStoragePin;
+  setPrivateKey;
+}
+
+interface CreateAccountState {
+  phase;
+}
+
+class CreateAccount extends Component<CreateAccountProps, CreateAccountState> {
+  state = {
+    phase: PHASES.RECORD_RECOVERY_PHRASE
+  };
+
+  render() {
+    const { handle, setPrivateKey, setStoragePin } = this.props;
+    return (
+      <div>
+        {this.state.phase === PHASES.RECORD_RECOVERY_PHRASE && (
+          <RecordRecoveryPhraseSlide
+            setPrivateKey={privateKey => {
+              setPrivateKey(privateKey);
+              this.setState({ phase: PHASES.RECORD_STORAGE_PIN });
+            }}
+          />
+        )}
+        {this.state.phase === PHASES.RECORD_STORAGE_PIN && (
+          <RecordStorageHandleSlide
+            handle={handle}
+            setStoragePin={storagePin => {
+              setStoragePin(storagePin);
+              this.setState({ phase: PHASES.SEND_PAYMENT });
+            }}
+          />
+        )}
+        {this.state.phase === PHASES.SEND_PAYMENT && <SendPaymentSlide />}
+        {this.state.phase === PHASES.CONFIRM_PAYMENT && <ConfirmPaymentSlide />}
+      </div>
+    );
+  }
+}
+
+export default CreateAccount;
