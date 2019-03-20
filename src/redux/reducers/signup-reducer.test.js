c4cb7aec584738650825d66b8613f62d62103dbe
Edmund Mai <edmundmai@gmail.com>
2019-03-20 14:45:39 -0400

    fix and add tests

--- src/redux/reducers/signup-reducer.test.js
+++ src/redux/reducers/signup-reducer.test.js
@@ -1,8 +1,11 @@
 import signup from "./signup-reducer";
 import signupActions from "../actions/signup-actions";
+import { SIGNUP_PHASES } from "../../config";
 
 const initState = {
-  privateKey: null
+  privateKey: null,
+  invoice: null,
+  phase: SIGNUP_PHASES.RECORD_RECOVERY_PHRASE
 };
 
 test("signup-reducer SET_PRIVATE_KEY", () => {
@@ -11,7 +14,35 @@ test("signup-reducer SET_PRIVATE_KEY", () => {
     payload: { privateKey: "foobar" }
   };
   const expected = {
-    privateKey: "foobar"
+    invoice: null,
+    privateKey: "foobar",
+    phase: SIGNUP_PHASES.RECORD_STORAGE_PIN
+  };
+  expect(signup(initState, action)).toEqual(expected);
+});
+
+test("signup-reducer GET_INVOICE_SUCCESS", () => {
+  const invoice = { cost: 123, ethAddress: "0x0x" };
+  const action = {
+    type: signupActions.GET_INVOICE_SUCCESS,
+    payload: { invoice }
+  };
+  const expected = {
+    invoice,
+    privateKey: null,
+    phase: SIGNUP_PHASES.SEND_PAYMENT
+  };
+  expect(signup(initState, action)).toEqual(expected);
+});
+
+test("signup-reducer ACCOUNT_PAID_SUCCESS", () => {
+  const action = {
+    type: signupActions.ACCOUNT_PAID_SUCCESS
+  };
+  const expected = {
+    invoice: null,
+    privateKey: null,
+    phase: SIGNUP_PHASES.CONFIRM_PAYMENT
   };
   expect(signup(initState, action)).toEqual(expected);
 });
