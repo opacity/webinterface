a544b68bc716be63089a5d699b74e751eb4f1660
Ladislav Balon <ladislav.balon@viptrust.com>
2019-03-20 16:37:16 +0100

    Stands out (#104)
    * Stands out
    
    * Added SVG icons
    
    * Code review fixes
    
    * Mobile version

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ import PaymentConfirm from "./components/payment-confirm";
 import PageNavigationPrompt from "./components/page-navigation-prompt";
 import Subscription from "./components/subscription";
 import Signup from "./components/signup";
+import StandsOut from "./components/stands-out";
 
 import ErrorPage from "./components/error-page";
 import ErrorTracker from "./services/error-tracker";
@@ -67,6 +68,7 @@ const App = () => (
             <Route path="/brokers-down" component={BrokersDown} />
             <Route path="/subscription" component={Subscription} />
             <Route path="/sign-up" component={Signup} />
+            <Route path="/stands-out" component={StandsOut} />
             <Route path="/file-manager" component={FileManager} />
             <Route path="/login-or-register" component={LoginOrRegister} />
             <Route path="/landing-page" component={LandingPage} />
