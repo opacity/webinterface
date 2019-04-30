c25105bc014043ff5510dd3b1c68350b0f593bf8
Edmund Mai <edmundmai@gmail.com>
2019-04-30 15:24:30 -0400

    remove old video

--- src/components/shared/video-player.tsx
+++ src/components/shared/video-player.tsx
@@ -5,7 +5,8 @@ import { Player } from "video-react";
 import "video-react/dist/video-react.css";
 
 const Container = styled.div`
-  width: 500px;
+  padding: 10px;
+  max-width: 500px;
   margin: 0 auto;
 `;
 
