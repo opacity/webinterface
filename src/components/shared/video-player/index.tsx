7aa6ce002874e1cd67e6087a2a1df82c4f0a99f8
Edmund Mai <edmundmai@gmail.com>
2019-04-30 15:20:49 -0400

    use video-react instead of react-media-player

--- src/components/shared/video-player/index.tsx
+++ src/components/shared/video-player/index.tsx
@@ -1,47 +1,22 @@
 import React, { Component } from "react";
-import { Media, Player /*, controls*/ } from "react-media-player";
-/*
-import PlayPause from "./PlayPause";
-import MuteUnmute from "./MuteUnmute";
-import Fullscreen from "./Fullscreen";
-*/
+import styled from "styled-components";
+import { Player } from "video-react";
 
-import "./main.scss";
+import "video-react/dist/video-react.css";
 
-/*
-const { CurrentTime, Progress, SeekBar, Duration, Volume } = controls;
-*/
+const Container = styled.div`
+  width: 500px;
+  margin: 0 auto;
+`;
 
 class VideoPlayer extends Component<any, any> {
-  render () {
+  render() {
+    console.log(this.props);
     const { src } = this.props;
     return (
-      <Media>
-        {({ isFullscreen, playPause }) => (
-          <div
-            className={
-              "media-player" + (isFullscreen ? " media-player--fullscreen" : "")
-            }
-            tabIndex={0}
-          >
-            <Player autoPlay={true} src={src} onClick={() => playPause()} />
-            {/*
-              <div className="media-controls">
-                <PlayPause className="media-control media-control--play-pause" />
-                <CurrentTime className="media-control media-control--current-time" />
-                <div className="media-control-group media-control-group--seek">
-                  <Progress className="media-control media-control--progress" />
-                  <SeekBar className="media-control media-control--seekbar" />
-                </div>
-                <Duration className="media-control media-control--duration" />
-                <MuteUnmute className="media-control media-control--mute-unmute" />
-                <Volume className="media-control media-control--volume" />
-                <Fullscreen className="media-control media-control--fullscreen" />
-              </div>
-            */}
-          </div>
-        )}
-      </Media>
+      <Container>
+        <Player playsInline autoPlay src={src} preload="auto" />
+      </Container>
     );
   }
 }
