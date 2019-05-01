58a61edb67cf8430bdf24ca107abf26e94661726
Edmund Mai <edmundmai@gmail.com>
2019-05-01 11:58:17 -0400

    fix layout issues on team and stands out pages

--- src/components/shared/video-player.tsx
+++ src/components/shared/video-player.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import { Player } from "video-react";
 
+import { MOBILE_WIDTH } from "../../config";
+
 import "video-react/dist/video-react.css";
 
 const Container = styled.div`
   padding: 10px;
-  max-width: 500px;
+  max-width: 800px;
   margin: 0 auto;
+
+  @media only screen and (max-width: ${MOBILE_WIDTH}px) {
+    max-width: 100%;
+  }
 `;
 
 interface VideoPlayerProps {
