aaaf955b65b91f1a00eafc870137eba1eddf9638
Edmund Mai <edmundmai@gmail.com>
2019-05-02 14:48:36 -0400

    reducers for upload

--- src/redux/reducers/index.tsx
+++ src/redux/reducers/index.tsx
@@ -1,13 +1,15 @@
 import { routerReducer } from "react-router-redux";
 import { combineReducers } from "redux";
 
-import coinMarketCap from "../reducers/coinmarketcap-reducer";
-import signup from "../reducers/signup-reducer";
-import authentication from "../reducers/authentication-reducer";
+import coinMarketCap from "./coinmarketcap-reducer";
+import signup from "./signup-reducer";
+import authentication from "./authentication-reducer";
+import upload from "./upload-reducer";
 
 export default combineReducers({
   coinMarketCap,
   signup,
   authentication,
+  upload,
   router: routerReducer
 });
