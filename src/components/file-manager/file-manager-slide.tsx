78f9e23e8fdaaad1f4bc8e3c4aff70d9f22cf80f
Ladislav Balon <ladislav.balon@viptrust.com>
2019-05-03 17:11:52 +0200

    color arrow

--- src/components/file-manager/file-manager-slide.tsx
+++ src/components/file-manager/file-manager-slide.tsx
@@ -195,14 +195,14 @@ const ArrowTop = styled(Arrow)`
   top: -10px;
   border-left: 5px solid transparent;
   border-right: 5px solid transparent;
-  border-bottom: 5px solid black;
+  border-bottom: 5px solid #687892;
 `;
 
 const ArrowDown = styled(Arrow)`
   top: 10px;
   border-left: 5px solid transparent;
   border-right: 5px solid transparent;
-  border-top: 5px solid black;
+  border-top: 5px solid #687892;
 `;
 
 const TableHeader = ({ param, title, sortBy, paramArrow }) => {
