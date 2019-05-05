df1a3460a3b7f186283587dd4171a65d0f8881ef
Ladislav Balon <ladislav.balon@viptrust.com>
2019-05-05 09:17:37 +0200

    prettier

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -226,15 +226,15 @@ class Header extends React.Component<HeaderProps, HeaderState> {
     menuOpen: false
   };
 
-  hamburgerClick() {
+  hamburgerClick () {
     this.setState({ menuOpen: true });
   }
 
-  closeClick() {
+  closeClick () {
     this.setState({ menuOpen: false });
   }
 
-  render() {
+  render () {
     const { type } = this.props;
     const { menuOpen } = this.state;
     return (
