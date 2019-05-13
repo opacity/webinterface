90878204790b507321c0e990f3cf2c3ba8fb14bb
Edmund Mai <edmundmai@gmail.com>
2019-05-13 16:37:07 -0400

    grab metadata

--- src/components/share-page/main.tsx
+++ src/components/share-page/main.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { ToastContainer } from "react-toastify";
+import { Download } from "opaque";
 
 import { HEADER_TYPES, theme } from "../../config";
 
@@ -10,13 +11,30 @@ const Container = styled.div`
   width: 100%;
 `;
 
+interface FileMetadata {
+  name?: string;
+  size?: number;
+}
+
 const Main = ({ handle, download }) => {
-  console.log(handle);
+  const [metadata, setMetadata] = useState<FileMetadata>({});
+
+  useEffect(() => {
+    const download = new Download(handle, {
+      endpoint: "http://176.9.147.13:8081",
+      autoStart: false
+    });
+
+    download.metadata.then(({ name, size }) => {
+      setMetadata({ name, size } as FileMetadata);
+    });
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Header type={HEADER_TYPES.SCREEN_CONTAINER} />
+        <div>{JSON.stringify(metadata)}</div>
         <button onClick={() => download(handle)}>Download file</button>
         <ToastContainer
           pauseOnHover={false}
