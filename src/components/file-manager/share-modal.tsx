a2e58c32f8d2a08b55c7b28c042f7815f535ac79
Edmund Mai <edmundmai@gmail.com>
2019-05-14 14:45:34 -0400

    fix modal styles on mobile

--- src/components/file-manager/share-modal.tsx
+++ src/components/file-manager/share-modal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
-// import Modal from "react-modal";
 import Modal, { ModalProvider } from "styled-react-modal";
 
 import { FRONT_END_URL, MOBILE_WIDTH, theme } from "../../config";
@@ -8,32 +7,28 @@ import { FRONT_END_URL, MOBILE_WIDTH, theme } from "../../config";
 import ClipboardWidget from "../shared/clipboard-widget";
 
 const Body = styled.div`
-  align-items: center;
   display: flex;
   justify-content: center;
   flex-direction: column;
+  @media (max-width: ${MOBILE_WIDTH}px) {
+    width: 290px;
+  }
 `;
 
 const StyledModal = Modal.styled`
   background-color: white;
   padding: 20px;
-  @media (max-width: ${MOBILE_WIDTH}px) {
-    padding: 0px 54px 54px 54px;
-  }
 `;
 
-// const FadingBackground = styled(BaseModalBackground)`
-// @media (max-width: ${MOBILE_WIDTH}px) {
-// background-color: white;
-// }
-// `;
-
-// const Title = styled.h3`
-// font-size: 20px;
-// `;
+const Title = styled.h2`
+  font-size: 20px;
+  color: ${props => props.theme.title.color};
+`;
 
 const Filename = styled.h3`
   font-size: 20px;
+  font-weight: 500;
+  margin-top: 0px;
 `;
 
 const ShareModal = ({ close, isOpen, file }) => (
@@ -45,6 +40,7 @@ const ShareModal = ({ close, isOpen, file }) => (
         onEscapeKeydown={() => close()}
       >
         <Body>
+          <Title>Share your file with others</Title>
           <Filename>{file && file.filename}</Filename>
           <ClipboardWidget
             text={`${FRONT_END_URL}/share?handle=${file && file.handle}`}
