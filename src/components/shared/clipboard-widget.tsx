bcaa66ba14aea22e22f05cb2009b4ee58c701809
Edmund Mai <edmundmai@gmail.com>
2019-05-15 13:34:52 -0400

    lint

--- src/components/shared/clipboard-widget.tsx
+++ src/components/shared/clipboard-widget.tsx
@@ -107,7 +107,7 @@ interface ClipboardWidgetProps {
 class ClipboardWidget extends Component<ClipboardWidgetProps> {
   state = { isCopied: false };
 
-  render() {
+  render () {
     const { text, title, property } = this.props;
 
     return (
