2a09bda3e1b7fd535b4f62d77a4685302a89159b
Edmund <edmundmai@gmail.com>
2019-05-16 16:07:51 -0400

    Fix missing default metadata issue (#176)
    * Fix missing default metadata issue
    
    * remove delete link
    
    * point to working opaque branch
    
    * again
    
    * lint

--- src/services/metadata.ts
+++ src/services/metadata.ts
@@ -1,13 +1,19 @@
 import CryptoJS from "crypto-js";
 
+const metadataGenerator = () => ({ files: [] });
+
 export const encrypt = (metadataKey, metadata) => {
   return CryptoJS.AES.encrypt(JSON.stringify(metadata), metadataKey).toString();
 };
 
 export const decrypt = (metadataKey, metadata) => {
-  const decrypted = CryptoJS.AES.decrypt(metadata, metadataKey);
-  const jsonString = decrypted.toString(CryptoJS.enc.Utf8);
-  return JSON.parse(jsonString);
+  try {
+    const decrypted = CryptoJS.AES.decrypt(metadata, metadataKey);
+    const jsonString = decrypted.toString(CryptoJS.enc.Utf8);
+    return JSON.parse(jsonString);
+  } catch (e) {
+    return metadataGenerator();
+  }
 };
 
 export default {
