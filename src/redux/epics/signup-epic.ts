a95ffb599a920d28aa27b5960551d2577700f8cf
Edmund Mai <edmundmai@gmail.com>
2019-05-17 15:05:42 -0400

    fix sign up payments

--- src/redux/epics/signup-epic.ts
+++ src/redux/epics/signup-epic.ts
@@ -1,43 +1,8 @@
-import { from, of, timer } from "rxjs";
-import {
-  map,
-  switchMap,
-  catchError,
-  takeUntil,
-  mergeMap,
-  filter
-} from "rxjs/operators";
+import { from, of } from "rxjs";
+import { map, switchMap, catchError } from "rxjs/operators";
 import { ofType, combineEpics } from "redux-observable";
 
 import signupActions from "../actions/signup-actions";
-
-// const createAccountEpic = (action$, state$, dependencies$) =>
-// action$.pipe(
-// ofType(signupActions.GET_INVOICE_PENDING),
-// switchMap(({ payload }) => {
-// const { masterHandle } = payload;
-
-// const md = forge.md.sha256.create();
-// md.update(privateKey + storagePin);
-
-// const accountId = Account.getAccountId({ privateKey, storagePin });
-// const metadataKey = Account.getMetadataKey({ privateKey, storagePin });
-// const storageLimit = 100;
-// const durationInMonths = 12;
-
-// return from(
-// backend.createAccount({
-// accountId,
-// storageLimit,
-// durationInMonths,
-// metadataKey
-// })
-// ).pipe(
-// map(invoice => signupActions.getInvoiceSuccess({ accountId, invoice })),
-// catchError(error => of(signupActions.getInvoiceFailure({ error })))
-// );
-// })
-// );
 
 const pollPaymentEpic = (action$, state$, dependencies$) =>
   action$.pipe(
@@ -45,18 +10,9 @@ const pollPaymentEpic = (action$, state$, dependencies$) =>
     switchMap(({ payload }) => {
       const { waitForPaymentFn } = payload;
 
-      const INITIAL_DELAY_MS = 5000;
-      const PERIODIC_DELAY_MS = 5000;
-
-      return timer(INITIAL_DELAY_MS, PERIODIC_DELAY_MS).pipe(
-        takeUntil(action$.ofType(signupActions.ACCOUNT_PAID_SUCCESS)),
-        mergeMap(() =>
-          from(waitForPaymentFn).pipe(
-            filter(isPaid => !!isPaid),
-            map(invoice => signupActions.accountPaidSuccess()),
-            catchError(error => of(signupActions.accountPaidFailure({ error })))
-          )
-        )
+      return from(waitForPaymentFn).pipe(
+        map(invoice => signupActions.accountPaidSuccess()),
+        catchError(error => of(signupActions.accountPaidFailure({ error })))
       );
     })
   );
