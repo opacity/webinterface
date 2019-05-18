5588af5b4976a8e48e833d9058f7c870bacabca6
Edmund Mai <edmundmai@gmail.com>
2019-05-18 11:27:38 -0400

    lint

--- src/components/signup/record-account-handle-slide.tsx
+++ src/components/signup/record-account-handle-slide.tsx
@@ -116,7 +116,7 @@ class RecordAccountHandleSlide extends Component<
     isCopied: false
   };
 
-  render() {
+  render () {
     const { handle, next, goBack } = this.props;
 
     return (
@@ -169,7 +169,7 @@ class RecordAccountHandleSlide extends Component<
                 this.state.isCopied
                   ? next()
                   : alert(
-                      "Please make sure to copy your account handle before proceeding."
+                    "Please make sure to copy your account handle before proceeding."
                     );
               }}
             >
