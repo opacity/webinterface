708f1c798886c66bed80502098e2c2c1dde69bee
Edmund Mai <edmundmai@gmail.com>
2019-05-30 13:31:06 -0400

    signup epic tests

---
+++ src/redux/epics/signup-epic.test.js
@@ -0,0 +1,31 @@
+import { of } from "rxjs";
+import { asyncScheduler } from "rxjs";
+import "rxjs/add/operator/toArray";
+import { push } from "connected-react-router";
+
+import signupActions from "../actions/signup-actions";
+import signupEpic from "./signup-epic";
+
+test("pollPaymentEpic on success", done => {
+  const signup = ["foo", "bar"];
+  const waitForPaymentFn = jest.fn(() => Promise.resolve());
+  const action$ = of(signupActions.pollPayment({ waitForPaymentFn }));
+
+  signupEpic(action$).subscribe(actions => {
+    expect(actions).toEqual(signupActions.accountPaidSuccess());
+    done();
+  });
+});
+
+test("pollPaymentEpic on failure", done => {
+  const signup = ["foo", "bar"];
+  const waitForPaymentFn = jest.fn(() => Promise.reject("foobar"));
+  const action$ = of(signupActions.pollPayment({ waitForPaymentFn }));
+
+  signupEpic(action$).subscribe(actions => {
+    expect(actions).toEqual(
+      signupActions.accountPaidFailure({ error: "foobar" })
+    );
+    done();
+  });
+});
