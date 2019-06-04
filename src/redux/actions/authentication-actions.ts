0ccebee67aec8d31aec9973e6be703b039b778ed
Edmund Mai <edmundmai@gmail.com>
2019-06-04 15:22:02 -0400

    redux flow for account data

--- src/redux/actions/authentication-actions.ts
+++ src/redux/actions/authentication-actions.ts
@@ -2,12 +2,18 @@ const LOGIN_PENDING = "opacity/authentication/login-pending";
 const LOGIN_SUCCESS = "opacity/authentication/login-success";
 const LOGIN_FAILURE = "opacity/authentication/login-failure";
 const LOGOUT = "opacity/authentication/logout";
+const FETCH_ACCOUNT_DATA_SUCCESS =
+  "opacity/authentication/fetch-account-data-success";
+const FETCH_ACCOUNT_DATA_FAILURE =
+  "opacity/authentication/fetch-account-data-failure";
 
 const ACTIONS = Object.freeze({
   LOGIN_PENDING,
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
   LOGOUT,
+  FETCH_ACCOUNT_DATA_SUCCESS,
+  FETCH_ACCOUNT_DATA_FAILURE,
 
   loginPending: ({ privateKey }) => ({
     type: LOGIN_PENDING,
@@ -23,6 +29,14 @@ const ACTIONS = Object.freeze({
   }),
   logout: () => ({
     type: LOGOUT
+  }),
+  fetchAccountDataSuccess: ({ storageUsed, storageLimit, expirationDate }) => ({
+    type: FETCH_ACCOUNT_DATA_SUCCESS,
+    payload: { storageUsed, storageLimit, expirationDate }
+  }),
+  fetchAccountDataFailure: ({ error }) => ({
+    type: FETCH_ACCOUNT_DATA_FAILURE,
+    payload: { error }
   })
 });
 
--- src/redux/epics/files-epic.ts
+++ src/redux/epics/files-epic.ts
@@ -3,6 +3,7 @@ import { map, switchMap, catchError } from "rxjs/operators";
 import { ofType, combineEpics } from "redux-observable";
 
 import filesActions from "../actions/files-actions";
+import authenticationActions from "../actions/authentication-actions";
 import uploadActions from "../actions/upload-actions";
 import removeActions from "../actions/remove-actions";
 
@@ -26,4 +27,30 @@ const getFileListEpic = (action$, state$, dependencies$) =>
     })
   );
 
-export default combineEpics(getFileListEpic);
+const getAccountDataEpic = (action$, state$, dependencies$) =>
+  action$.pipe(
+    ofType(
+      filesActions.GET_FILE_LIST,
+      uploadActions.UPLOAD_SUCCESS,
+      removeActions.REMOVE_SUCCESS
+    ),
+    switchMap(({ payload }) => {
+      const { masterHandle } = payload;
+
+      return from(masterHandle.getAccountInfo()).pipe(
+        map((data: any) =>
+          authenticationActions.fetchAccountDataSuccess({
+            storageUsed: data.storageUsed,
+            storageLimit: data.storageLimit,
+            expirationDate: data.expirationDate
+          })
+        ),
+        catchError(error => {
+          console.log("ERROR: ", error);
+          return of(authenticationActions.fetchAccountDataFailure({ error }));
+        })
+      );
+    })
+  );
+
+export default combineEpics(getFileListEpic, getAccountDataEpic);
