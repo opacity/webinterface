0c65208aaf1fc658491539c01288e373cfea190e
Ladislav Balon <ladislav.balon@viptrust.com>
2019-06-17 14:34:11 +0200

    Bio

--- src/components/team-page/team-page-slide.tsx
+++ src/components/team-page/team-page-slide.tsx
@@ -25,63 +25,80 @@ const people = [
     icon: ICON_JASON,
     role: "CEO",
     github: "https://github.com/funkydrummer",
-    linkedin: "https://www.linkedin.com/in/jasoncoppola"
+    linkedin: "https://www.linkedin.com/in/jasoncoppola",
+    bio:
+      "Jason has 20 years experience in software product  development, working in Engineering, Product, and Executive leadership. He has led organizations to build and deliver highly scalable applications at Fortune 100 and startup companies by applying his experience as an entrepreneur and Agile expert."
   },
   {
     title: "Aron Hiltzik",
     icon: ICON_ARON,
     role: "Marketing",
     github: "",
-    linkedin: "https://www.linkedin.com/in/aronhiltzik/"
+    linkedin: "https://www.linkedin.com/in/aronhiltzik/",
+    bio:
+      "Aron is a recent Student-athlete graduate from University of Illinois with a Bachelor of Arts in Advertising. He founded his first business in 2018, which scaled to over $500k in revenue in 6 months. Aron has specialized himself in Marketing, SEO optimization, and brand strategy."
   },
   {
     title: "Tim DeHaas",
     icon: ICON_TIM,
     role: "Community Manager",
     github: "https://github.com/MrRedPandabaer/",
-    linkedin: ""
+    linkedin: "",
+    bio:
+      "Tim has 2 years of experience in building online communities and social media management while utilizing different platforms to magnify outreach. He also possesses a skill set in basic programming coming handy in community interactions. Further, Tim has a B. Sc. in Biochemistry and is currently taking a Master's degree in Biochemistry and Molecular Biology."
   },
   {
     title: "William Halunen",
     icon: ICON_WILLIAM,
     role: "Community Manager",
     github: "https://github.com/WHalunen",
-    linkedin: "https://www.linkedin.com/in/william-halunen-080987156/"
+    linkedin: "https://www.linkedin.com/in/william-halunen-080987156/",
+    bio:
+      "William has 2 years of experience managing online communities. He has helped organically grow communities totaling over 50,000 members across all social media outlets. He also has a bachelors degree in computer science."
   },
   {
     title: "Connor Hen",
     icon: ICON_CONNOR,
     role: "Developer",
     github: "https://github.com/CKH4",
-    linkedin: ""
+    linkedin: "",
+    bio:
+      "Connor has spent the past 5 years in a variety of development roles. He got his programming start in game development making arcade-style games before moving on to generative art and frontend web development. Since then he's moved into a more general role doing everything from designing and building frontends to library development."
   },
   {
     title: "Marcel Lindig",
     icon: ICON_MARCEL,
     role: "Developer",
     github: "https://github.com/nullpilot",
-    linkedin: ""
+    linkedin: "",
+    bio:
+      "Marcel has 10 years of experience developing software, working in everything from front end development to database administration. He has worked with a variety of clients, from individual designers to international corporations, to advise on software needs and deliver custom solutions."
   },
   {
     title: "Edmund Mai",
     icon: ICON_EDMUND,
     role: "Software Engineer",
     github: "https://github.com/EdmundMai",
-    linkedin: "https://www.linkedin.com/in/edmundmai/"
+    linkedin: "https://www.linkedin.com/in/edmundmai/",
+    bio:
+      "Edmund is an experienced lead developer with experience working for renowned tech startups in NYC. He has founded and built multiple software businesses, one of which was recently acquired."
   },
   {
     title: "Rebel Fornea",
     icon: ICON_REBEL,
     role: "Technical Lead",
     github: "https://github.com/rfornea",
-    linkedin: "https://www.linkedin.com/in/rebel-fornea-7640b8122/"
+    linkedin: "https://www.linkedin.com/in/rebel-fornea-7640b8122/",
+    bio:
+      "Rebel began her software career in the frontend in a typical corporation. She got her start in crypto by jointly winning a developer bounty using a language she'd never developed in, and now focuses on backend development, deployments, and design."
   },
   {
     title: "Ladislav Balon",
     icon: ICON_LADISLAV,
     role: "Fullstack Developer",
     github: "https://github.com/tenisakb",
-    linkedin: "https://www.linkedin.com/in/ladislav-balon-00134b54/"
+    linkedin: "https://www.linkedin.com/in/ladislav-balon-00134b54/",
+    bio: "Ladislav is experienced full-stack developer with an overlap, he started programming when he was 13 years old. He has worked on many commercial projects for interesting clients as well as on his own projects."
   }
 ];
 
@@ -198,6 +215,18 @@ const PersonRole = styled(Parapraph)`
   color: #4f5e78;
 `;
 
+const PersonBio = styled(Parapraph)`
+  font-size: 12px;
+  width: 170px;
+  margin-top: 10px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: 0.3px;
+  color: #4f5e78;
+`;
+
 const Content = styled(Parapraph)`
   font-size: 16px;
   letter-spacing: ${props => props.theme.letterSpacing};
@@ -232,6 +261,7 @@ const TeamPageSlide = ({ isLoggedIn }) => (
               </Wrapper>
               <PersonTitle>{person.title}</PersonTitle>
               <PersonRole>{person.role}</PersonRole>
+              <PersonBio>{person.bio}</PersonBio>
               {(person.linkedin || person.github) && (
                 <PersonLinkContainer>
                   {person.linkedin && (
