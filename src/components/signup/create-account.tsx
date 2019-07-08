0c5b34d6c78e734e56101ef57c68f09408b8b1de
Edmund Mai <edmundmai@gmail.com>
2019-07-08 12:16:56 -0400

    clean up styling

--- src/components/signup/create-account.tsx
+++ src/components/signup/create-account.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styled, { ThemeProvider } from "styled-components";
+import styled from "styled-components";
 
 import Breadcrumbs from "./breadcrumbs";
 import SelectPlanSlide from "./select-plan-slide";
@@ -10,7 +10,7 @@ import ConfirmPaymentSlide from "./confirm-payment-slide";
 import ScreenContainer from "../shared/screen-container";
 import Header from "../shared/header";
 
-import { OPAQUE, HEADER_TYPES, SIGNUP_PHASES, theme } from "../../config";
+import { OPAQUE, HEADER_TYPES, SIGNUP_PHASES } from "../../config";
 
 import { Account, MasterHandle } from "opaque";
 
@@ -80,50 +80,48 @@ const CreateAccount = ({
   );
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Header type={HEADER_TYPES.EMPTY} />
-        <ScreenContainer
-          title={
-            plan
-              ? `Register on Opacity: ${plan.title} Plan ${plan.storageLimit}`
-              : "Register on Opacity"
-          }
-        >
-          <Breadcrumbs phase={phase} />
-          {phase === SIGNUP_PHASES.SELECT_PLAN && <SelectPlanSlide />}
-          {phase === SIGNUP_PHASES.RECORD_RECOVERY_PHRASE && (
-            <RecordRecoveryPhraseSlide mnemonic={mnemonic} next={showAddress} />
-          )}
-          {phase === SIGNUP_PHASES.RECORD_STORAGE_PIN && (
-            <RecordAccountHandleSlide
-              handle={privateKey}
-              next={() => pollPayment(waitForPaymentFn)}
-              back={() => showMnemonic()}
-            />
-          )}
-          {phase === SIGNUP_PHASES.SEND_PAYMENT && (
-            <SendPaymentSlide
-              cost={12}
-              invoice={invoice}
-              openMetamask={openMetamask}
-              fiatPaymentError={fiatPaymentError}
-              fiatPaymentStatus={fiatPaymentStatus}
-              payFiat={stripeToken =>
-                payFiat({
-                  stripeToken,
-                  masterHandle,
-                  timestamp: new Date().valueOf()
-                })
-              }
-            />
-          )}
-          {phase === SIGNUP_PHASES.CONFIRM_PAYMENT && (
-            <ConfirmPaymentSlide handle={privateKey} />
-          )}
-        </ScreenContainer>
-      </Container>
-    </ThemeProvider>
+    <Container>
+      <Header type={HEADER_TYPES.EMPTY} />
+      <ScreenContainer
+        title={
+          plan
+            ? `Register on Opacity: ${plan.title} Plan ${plan.storageLimit}`
+            : "Register on Opacity"
+        }
+      >
+        <Breadcrumbs phase={phase} />
+        {phase === SIGNUP_PHASES.SELECT_PLAN && <SelectPlanSlide />}
+        {phase === SIGNUP_PHASES.RECORD_RECOVERY_PHRASE && (
+          <RecordRecoveryPhraseSlide mnemonic={mnemonic} next={showAddress} />
+        )}
+        {phase === SIGNUP_PHASES.RECORD_STORAGE_PIN && (
+          <RecordAccountHandleSlide
+            handle={privateKey}
+            next={() => pollPayment(waitForPaymentFn)}
+            back={() => showMnemonic()}
+          />
+        )}
+        {phase === SIGNUP_PHASES.SEND_PAYMENT && (
+          <SendPaymentSlide
+            cost={plan.cost}
+            invoice={invoice}
+            openMetamask={openMetamask}
+            fiatPaymentError={fiatPaymentError}
+            fiatPaymentStatus={fiatPaymentStatus}
+            payFiat={stripeToken =>
+              payFiat({
+                stripeToken,
+                masterHandle,
+                timestamp: new Date().valueOf()
+              })
+            }
+          />
+        )}
+        {phase === SIGNUP_PHASES.CONFIRM_PAYMENT && (
+          <ConfirmPaymentSlide handle={privateKey} />
+        )}
+      </ScreenContainer>
+    </Container>
   );
 };
 
--- src/components/signup/credit-card-form.tsx
+++ src/components/signup/credit-card-form.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { injectStripe, CardElement } from "react-stripe-elements";
 import { CountryDropdown } from "react-country-region-selector";
 import { Form, Field } from "react-final-form";
 
 import OutboundLink from "../shared/outbound-link";
 
-import { FIAT_PAYMENT_STATUSES } from "../../config";
+import { FIAT_PAYMENT_STATUSES, theme } from "../../config";
 
 interface IInputProps {
   invalid: boolean;
@@ -18,7 +18,8 @@ const TextInput = styled.input.attrs<IInputProps>({
   background: white;
   font-family: "Lato", sans-serif;
   border-radius: 3px;
-  border: 1px solid ${props => (props.invalid ? "#ff3860" : "transparent")};
+  border: 1px solid
+    ${props => (props.invalid ? props.theme.error.color : "transparent")};
   font-size: 16px;
   padding: 10px;
 
@@ -31,12 +32,13 @@ const Checkbox = styled.input.attrs<IInputProps>({
   type: "checkbox"
 })`
   padding: 2px;
-  border: 1px solid ${props => (props.invalid ? "#ff3860" : "transparent")};
+  border: 1px solid
+    ${props => (props.invalid ? props.theme.error.color : "transparent")};
 `;
 
-const SelectDropdown = styled(CountryDropdown)`
+const SelectDropdown = styled(CountryDropdown)<IInputProps>`
   border: 1px solid
-    ${(props: IInputProps) => (props.invalid ? "#ff3860" : "transparent")};
+    ${props => (props.invalid ? props.theme.error.color : "transparent")};
   background: white;
   height: 36px;
   font-size: 16px;
@@ -170,120 +172,125 @@ const CreditCardForm = ({ cost, stripe, onSubmit, error, status }) => {
   };
 
   return (
-    <Form
-      onSubmit={submit}
-      render={({ handleSubmit, pristine, invalid }) => (
-        <form onSubmit={handleSubmit}>
-          <Row>
-            <Group>
-              <Field name="firstName" validate={required}>
+    <ThemeProvider theme={theme}>
+      <Form
+        onSubmit={submit}
+        render={({ handleSubmit, pristine, invalid }) => (
+          <form onSubmit={handleSubmit}>
+            <Row>
+              <Group>
+                <Field name="firstName" validate={required}>
+                  {({ input, meta }) => (
+                    <Label>
+                      <InputName>First Name</InputName>
+                      <TextInput
+                        {...input}
+                        disabled={isSubmitDisabled}
+                        placeholder="First Name"
+                        invalid={meta.touched && meta.error}
+                      />
+                    </Label>
+                  )}
+                </Field>
+              </Group>
+              <Group>
+                <Field name="lastName" validate={required}>
+                  {({ input, meta }) => (
+                    <Label>
+                      <InputName>Last Name</InputName>
+                      <TextInput
+                        {...input}
+                        disabled={isSubmitDisabled}
+                        placeholder="Last Name"
+                        invalid={meta.touched && meta.error}
+                      />
+                    </Label>
+                  )}
+                </Field>
+              </Group>
+            </Row>
+            <Row>
+              <Group>
+                <Label>
+                  <InputName>Card Details</InputName>
+                  <CardElement
+                    disabled={isSubmitDisabled}
+                    style={{
+                      base: {
+                        fontSize: "16px",
+                        fontWeight: "normal",
+                        fontFamily: '"Lato", sans-serif'
+                      }
+                    }}
+                    classes={{
+                      base: "stripe-cc-input",
+                      invalid: "stripe-cc-input-error"
+                    }}
+                  />
+                </Label>
+              </Group>
+            </Row>
+            <Row>
+              <Group>
+                <Field name="billingCountry" validate={required}>
+                  {({ input, meta }) => (
+                    <Label>
+                      <InputName>Billing Country</InputName>
+                      <SelectDropdown
+                        {...input}
+                        disabled={isSubmitDisabled}
+                        priorityOptions={["US"]}
+                        valueType="short"
+                        invalid={meta.touched && meta.error ? 1 : 0}
+                      />
+                    </Label>
+                  )}
+                </Field>
+              </Group>
+            </Row>
+            <Row>
+              <SubmitSection>
+                <SubmitOrderButton disabled={invalid || isSubmitDisabled}>
+                  {isSubmitDisabled ? "Processing..." : "Purchase"}
+                </SubmitOrderButton>
+                {error && (
+                  <ErrorMessage>
+                    We were unable to process your payment. Please try again
+                    later.
+                  </ErrorMessage>
+                )}
+              </SubmitSection>
+            </Row>
+            <Row>
+              <Field name="isTermsChecked" validate={required} type="checkbox">
                 {({ input, meta }) => (
-                  <Label>
-                    <InputName>First Name</InputName>
-                    <TextInput
-                      {...input}
-                      disabled={isSubmitDisabled}
-                      placeholder="First Name"
-                      invalid={meta.touched && meta.error}
-                    />
-                  </Label>
+                  <Group>
+                    <AgreementLabel>
+                      <Checkbox
+                        {...input}
+                        invalid={meta.touched && meta.error}
+                      />
+                      <AgreementText>
+                        I agree to the{" "}
+                        <OutboundLink href="/terms-of-service">
+                          Terms and Conditions
+                        </OutboundLink>
+                      </AgreementText>
+                    </AgreementLabel>
+                    {meta.touched &&
+                      meta.error && (
+                        <ErrorMessage>
+                          Please check this box if you want to proceed.
+                        </ErrorMessage>
+                      )}
+                  </Group>
                 )}
               </Field>
-            </Group>
-            <Group>
-              <Field name="lastName" validate={required}>
-                {({ input, meta }) => (
-                  <Label>
-                    <InputName>Last Name</InputName>
-                    <TextInput
-                      {...input}
-                      disabled={isSubmitDisabled}
-                      placeholder="Last Name"
-                      invalid={meta.touched && meta.error}
-                    />
-                  </Label>
-                )}
-              </Field>
-            </Group>
-          </Row>
-          <Row>
-            <Group>
-              <Label>
-                <InputName>Card Details</InputName>
-                <CardElement
-                  disabled={isSubmitDisabled}
-                  style={{
-                    base: {
-                      fontSize: "16px",
-                      fontWeight: "normal",
-                      fontFamily: '"Lato", sans-serif'
-                    }
-                  }}
-                  classes={{
-                    base: "stripe-cc-input",
-                    invalid: "stripe-cc-input-error"
-                  }}
-                />
-              </Label>
-            </Group>
-          </Row>
-          <Row>
-            <Group>
-              <Field name="billingCountry" validate={required}>
-                {({ input, meta }) => (
-                  <Label>
-                    <InputName>Billing Country</InputName>
-                    <SelectDropdown
-                      {...input}
-                      disabled={isSubmitDisabled}
-                      priorityOptions={["US"]}
-                      valueType="short"
-                      invalid={meta.touched && meta.error ? 1 : 0}
-                    />
-                  </Label>
-                )}
-              </Field>
-            </Group>
-          </Row>
-          <Row>
-            <SubmitSection>
-              <SubmitOrderButton disabled={invalid || isSubmitDisabled}>
-                {isSubmitDisabled ? "Processing..." : "Purchase"}
-              </SubmitOrderButton>
-              {error && (
-                <ErrorMessage>
-                  We were unable to process your payment. Please try again
-                  later.
-                </ErrorMessage>
-              )}
-            </SubmitSection>
-          </Row>
-          <Row>
-            <Field name="isTermsChecked" validate={required} type="checkbox">
-              {({ input, meta }) => (
-                <Group>
-                  <AgreementLabel>
-                    <Checkbox {...input} invalid={meta.touched && meta.error} />
-                    <AgreementText>
-                      I agree to the{" "}
-                      <OutboundLink href="/terms-of-service">
-                        Terms and Conditions
-                      </OutboundLink>
-                    </AgreementText>
-                  </AgreementLabel>
-                  {meta.touched &&
-                    meta.error && (
-                      <ErrorMessage>
-                        Please check this box if you want to proceed.
-                      </ErrorMessage>
-                    )}
-                </Group>
-              )}
-            </Field>
-          </Row>
-        </form>
-      )}
-    />
+            </Row>
+          </form>
+        )}
+      />
+    </ThemeProvider>
   );
 };
 
