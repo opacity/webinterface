747a8a19bb617e490047bc897f88db523999aa13
Edmund Mai <edmundmai@gmail.com>
2019-08-07 14:45:15 -0400

    lint

--- src/components/file-manager/file-manager-slide.tsx
+++ src/components/file-manager/file-manager-slide.tsx
@@ -428,11 +428,11 @@ const FileManagerSlide = ({
     !isExist
       ? createFolder(masterHandle, currentFolder, name)
       : toast(`Folder ${name} is found. `, {
-          autoClose: 3000,
-          hideProgressBar: true,
-          position: toast.POSITION.BOTTOM_RIGHT,
-          toastId: name
-        });
+        autoClose: 3000,
+        hideProgressBar: true,
+        position: toast.POSITION.BOTTOM_RIGHT,
+        toastId: name
+      });
   };
 
   useEffect(
@@ -636,12 +636,12 @@ const FileManagerSlide = ({
                               onChange={e =>
                                 e.target.checked
                                   ? selectFile({
-                                      name,
-                                      handle,
-                                      size,
-                                      created,
-                                      version
-                                    })
+                                    name,
+                                    handle,
+                                    size,
+                                    created,
+                                    version
+                                  })
                                   : deselectFile(handle)
                               }
                             />
