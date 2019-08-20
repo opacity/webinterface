ce8564f37ec1321d21dd16c6debec8ad00a08db8
Ladislav Balon <ladislav.balon@viptrust.com>
2019-08-20 09:07:23 +0200

    Fix - all comments + improvements

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
-import { withRouter } from "react-router";
 
 import { theme, MOBILE_WIDTH } from "../../config";
 
-// const ICON_GITHUB = require("../../assets/images/github.svg");
-// const ICON_TELEGRAM = require("../../assets/images/telegram.svg");
-// const ICON_REDDIT = require("../../assets/images/reddit.svg");
-// const ICON_TWITTER = require("../../assets/images/twitter.svg");
-// const ICON_YOUTUBE = require("../../assets/images/youtube.svg");
 const ICON_ARROW_RIGHT = require("../../assets/images/arrow_right.svg");
 
 const Container = styled.div`
@@ -24,16 +18,6 @@ const Column = styled.div`
 const Wrapper = styled.div`
   background-color: #2e6dde;
   padding-bottom: 20px;
-`;
-
-const IconArrow = styled.img`
-  display: none;
-  width: 20px;
-  color: white;
-  @media only screen and (max-width: ${MOBILE_WIDTH}px) {
-    display: inline-block;
-    float: right;
-  }
 `;
 
 const Link = styled.a`
@@ -52,10 +36,10 @@ const Link = styled.a`
     opacity: 0.8;
   }
   @media only screen and (max-width: ${MOBILE_WIDTH}px) {
-    padding: 50px;
     font-size: 24px;
     border-top: 0.5px;
     border-bottom: 0.5px;
+    margin: 5px 0px 0px 50px;
   }
 `;
 
@@ -70,10 +54,10 @@ const Copyright = styled.span`
   color: white;
 
   @media only screen and (max-width: ${MOBILE_WIDTH}px) {
-    padding: 50px;
     font-size: 16px;
     border-top: 0.5px;
     border-bottom: 0.5px;
+    margin: 5px 0px 0px 50px;
   }
 `;
 
@@ -102,78 +86,118 @@ const BuySubContainer = styled(SubContainer)`
   justify-content: space-around;
 `;
 
-const Footer = ({ history }) => (
+const IconArrow = styled.img.attrs({
+  src: ICON_ARROW_RIGHT
+})`
+  display: none;
+  width: 20px;
+  color: white;
+  margin-right: 10px;
+  @media only screen and (max-width: ${MOBILE_WIDTH}px) {
+    display: inline-block;
+  }
+`;
+
+const Block = styled.div`
+  @media only screen and (max-width: ${MOBILE_WIDTH}px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Footer = () => (
   <ThemeProvider theme={theme}>
     <Wrapper>
       <Container>
-      <BuySubContainer>
-      <Column>
-        <FooterHeader> Opacity </FooterHeader>
-        <div><Copyright>Opacity © 2019</Copyright></div>
-        <div><Copyright>support@opacity.io</Copyright></div>
-      </Column>
-      <Column>
-      <FooterHeader> Company </FooterHeader>
-      <div><Link onClick={() => history.push("/terms-of-service")}>
-        Terms of Service
-      </Link></div>
-      <div> <Link onClick={() => history.push("/privacy-policy")}>
-        Privacy Policy
-      </Link> </div>
-      <div><Link onClick={() => history.push("/code-review-license")}>
-        Open Source
-      </Link></div>
-      </Column>
+        <BuySubContainer>
           <Column>
-          <FooterHeader> Social </FooterHeader>
-          <div>
-            <Link href="https://github.com/opacity" target="_blank">
-              Github <IconArrow src={ICON_ARROW_RIGHT} />
-            </Link>
-          </div>
-          <div>
-            <Link href="https://telegram.me/opacitystorage" target="_blank">
-               Telegram <IconArrow src={ICON_ARROW_RIGHT} />
-            </Link>
-          </div>
-          <div>
-            <Link href="https://www.reddit.com/r/Opacity/" target="_blank">
-              Reddit <IconArrow src={ICON_ARROW_RIGHT} />
-            </Link>
-          </div>
-          <div>
-            <Link href="https://twitter.com/Opacity_Storage"target="_blank">
-              Twitter <IconArrow src={ICON_ARROW_RIGHT} />
-            </Link>
-          </div>
-          <div>
-            <Link href="http://www.youtube.com/c/OpacityStorage" target="_blank">
-              YouTube <IconArrow src={ICON_ARROW_RIGHT} />
-            </Link>
-          </div>
-          <div>
-          <Link href="https://medium.com/opacity-storage" target="_blank">
-            Medium <IconArrow src={ICON_ARROW_RIGHT} />
-          </Link>
-          </div>
+            <FooterHeader>Opacity</FooterHeader>
+            <Block>
+              <Copyright>Opacity © 2019</Copyright>
+            </Block>
+            <Block>
+              <Copyright>support@opacity.io</Copyright>
+            </Block>
           </Column>
           <Column>
-          <FooterHeader> Learn </FooterHeader>
-          <div>
-            <Link href="https://www.youtube.com/watch?v=J-o6pW8uUtg" target="_blank">
-              What is OPQ? <IconArrow src={ICON_ARROW_RIGHT} />
-            </Link>
-          </div>
-          <div>
-            <Link href="https://medium.com/opacity-storage/new-to-opq-heres-how-to-get-it-8285819698e" target="_blank">
-              How to get OPQ <IconArrow src={ICON_ARROW_RIGHT} />
-            </Link>
-          </div>
+            <FooterHeader>Company</FooterHeader>
+            <Block>
+              <Link href="/terms-of-service">Terms of Service</Link>
+            </Block>
+            <Block>
+              <Link href="/privacy-policy">Privacy Policy</Link>{" "}
+            </Block>
+            <Block>
+              <Link href="/code-review-license">Open Source</Link>
+            </Block>
           </Column>
-          </BuySubContainer>
+          <Column>
+            <FooterHeader>Social</FooterHeader>
+            <Block>
+              <Link href="https://github.com/opacity" target="_blank">
+                Github
+              </Link>
+              <IconArrow />
+            </Block>
+            <Block>
+              <Link href="https://telegram.me/opacitystorage" target="_blank">
+                Telegram
+              </Link>
+              <IconArrow />
+            </Block>
+            <Block>
+              <Link href="https://www.reddit.com/r/Opacity/" target="_blank">
+                Reddit
+              </Link>
+              <IconArrow />
+            </Block>
+            <Block>
+              <Link href="https://twitter.com/Opacity_Storage" target="_blank">
+                Twitter
+              </Link>
+              <IconArrow />
+            </Block>
+            <Block>
+              <Link
+                href="http://www.youtube.com/c/OpacityStorage"
+                target="_blank"
+              >
+                YouTube
+              </Link>
+              <IconArrow />
+            </Block>
+            <Block>
+              <Link href="https://medium.com/opacity-storage" target="_blank">
+                Medium
+              </Link>
+              <IconArrow />
+            </Block>
+          </Column>
+          <Column>
+            <FooterHeader>Learn</FooterHeader>
+            <Block>
+              <Link
+                href="https://www.youtube.com/watch?v=J-o6pW8uUtg"
+                target="_blank"
+              >
+                What is OPQ?
+              </Link>
+              <IconArrow />
+            </Block>
+            <Block>
+              <Link
+                href="https://medium.com/opacity-storage/new-to-opq-heres-how-to-get-it-8285819698e"
+                target="_blank"
+              >
+                How to get OPQ
+              </Link>
+              <IconArrow />
+            </Block>
+          </Column>
+        </BuySubContainer>
       </Container>
     </Wrapper>
   </ThemeProvider>
 );
 
-export default withRouter(Footer);
+export default Footer;
