2d3ad75489a130e79d4ca4e0e1a2c985c84a5f5c
Connor <13987924+CKH4@users.noreply.github.com>
2020-10-16 05:57:18 -0400

    Pretty

--- src/components/swap/notice.tsx
+++ src/components/swap/notice.tsx
@@ -1,11 +1,10 @@
-import React from "react"
-import { Link } from "react-router-dom"
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SwapNotice = () => (
-	<div style={{ padding: "8px", backgroundColor: "#FADA5E", textAlign: "center" }}>
-		<p style={{ margin: "0" }}><em>Action Required:</em> <span>OPQ → OPCT Token Swap</span> <Link to="./swap-information">Learn More</Link></p>
-	</div>
+  <div style={{ padding: "8px", backgroundColor: "#FADA5E", textAlign: "center" }}>
+    <p style={{ margin: "0" }}><em>Action Required:</em> <span>OPQ → OPCT Token Swap</span> <Link to="./swap-information">Learn More</Link></p>
+  </div>
 );
 
 export default SwapNotice;
